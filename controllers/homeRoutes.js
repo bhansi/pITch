1165404b9d62913b7f6ef7d6de82fce99ef03b7b
Baljot Hansi <baljotshansi@gmail.com>
2023-11-24 09:26:17 -0400

    Created GET '/' route for homeRoutes.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,32 @@
 const router = require('express').Router();
+const { User, Post } = require('../models');
+
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: {
+        model: User,
+        attributes: {
+          exclude: [ 'password' ]
+        }
+      },
+      order: [
+        [ 'date_created', 'DESC' ]
+      ]
+    });
+
+    if(!postData.length) {
+      res.status(404).json({ message: 'No posts found.' });
+      return;
+    }
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.status(200).json({ posts });
+  }
+  catch(err) {
+    res.status(400).json({ err });
+  }
+});
 
 module.exports = router;
